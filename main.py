8854e2d5512e09911d943b5e4ba317486e88eb59
tawficFatah <tawficFatah@gmail.com>
2025-05-19 12:17:17 -0400

    added a Player class, moving and rotating logic, as well as using pygame containers

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     # initialize pygame
     init_results = pygame.init()
-    
-    # Debug init_results include successful and failed (s, f) modules that were loaded 
-    # print(init_results)
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    
+    # getting a pygame time clock object
+    clock = pygame.time.Clock()
+    
+    # all the objects that can be updated
+    updatable = pygame.sprite.Group()
+    # all the objects that can be drawn
+    drawable = pygame.sprite.Group()
+
+    player_x = SCREEN_WIDTH  / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
+
+    Player.containers = (updatable, drawable)
+
+    updatable.add(player)
+    drawable.add(player)
+
+    dt = 0
+    continue_running = True
+
+    while continue_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                continue_running = False
 
+        updatable.update(dt)
+        #player.update(dt)
+        
         screen.fill("black")
+
+        # drawing the player
+        for object in drawable:
+            object.draw(screen)
+            
+        #player.draw(screen)
+        
         pygame.display.flip()
 
-
+        # limit the framerate to 60 FPS        
+        dt = clock.tick() / 1000
+        
 if __name__ == "__main__":
     main()
